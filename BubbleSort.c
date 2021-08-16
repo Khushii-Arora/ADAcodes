24d9b688d615aecc6c58f662816ed7f28e2e60c3
Khushi Arora <akhushi72@gmail.com>
2021-08-16 10:34:20 +0530

    Bubble sort with clock impications

---
+++ BubbleSort.c
@@ -0,0 +1,52 @@
+
+
+                            
+
+
+#include <stdio.h>
+#include <time.h>
+
+int main()
+{
+    printf("Khushi Aroran \n CSE3c \n");
+     clock_t t; 
+
+    t = clock(); 
+    int array[100], n, c, d, swap;
+
+  printf("Enter number of elements\n");
+  scanf("%d", &n);
+
+  printf("Enter %d integers\n", n);
+
+  for (c = 0; c < n; c++)
+    scanf("%d", &array[c]);
+
+  for (c = 0 ; c < n - 1; c++)
+  {
+    for (d = 0 ; d < n - c - 1; d++)
+    {
+      if (array[d] > array[d+1]) 
+      {
+        swap       = array[d];
+        array[d]   = array[d+1];
+        array[d+1] = swap;
+      }
+    }
+  }
+
+  printf("Sorted list in ascending order:\n");
+
+  for (c = 0; c < n; c++)
+     printf("%d\n", array[c]);
+    
+    t = clock() - t; 
+
+    double time_taken = ((double)t)/CLOCKS_PER_SEC;
+
+    printf( "%f seconds taken to executen", time_taken); 
+ 
+    return 0;
+}
+   
+
